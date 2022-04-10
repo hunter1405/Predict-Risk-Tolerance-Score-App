4db1633331fda02f9354afcf10fcefd385c9a135
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-11 03:06:41 +0700

    Add files via upload

---
+++ Predict_Risk_Score_Model.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+from sklearn import tree
+from sklearn import neighbors
+from sklearn import linear_model
+from sklearn import svm
+import statsmodels.formula.api as smf
+
+df_full = pd.read_csv('C:/Users/ACER/FINAL.csv')
+df_full = df_full.drop('Unnamed: 0',axis=1)
+df_1 = df_full.loc[:,'Age':'Objective']
+df_2 = df_full.loc[:,'GL_1':]
+df_2['GL_1']=df_2['GL_1'].apply(lambda x: x.replace('A','4').replace('B','3').replace('C','2').replace('D','1'))
+df_2['GL_2']=df_2['GL_2'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+df_2['GL_3']=df_2['GL_3'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+df_2['GL_4']=df_2['GL_4'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3'))
+df_2['GL_5']=df_2['GL_5'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3'))
+df_2['GL_6']=df_2['GL_6'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+df_2['GL_7']=df_2['GL_7'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+df_2['GL_8']=df_2['GL_8'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+df_2['GL_9']=df_2['GL_9'].apply(lambda x: x.replace('A','1').replace('B','3'))
+df_2['GL_10']=df_2['GL_10'].apply(lambda x: x.replace('A','1').replace('B','3'))
+df_2['GL_11']=df_2['GL_11'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+df_2['GL_12']=df_2['GL_12'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3'))
+df_2['GL_13']=df_2['GL_13'].apply(lambda x: x.replace('A','1').replace('B','2').replace('C','3').replace('D','4'))
+for i in df_2.columns.tolist():
+    df_2[i] = df_2[i].apply(lambda x: int(x))
+df_1['Score'] = 0
+for i in range(df_2.shape[0]):
+    df_1['Score'].iloc[i,:]=sum(df_2.iloc[i,:])
+df=df_1.copy()
+df=pd.get_dummies(df)
+
+
+# Separating X and y
+X=df.drop(['Score','Experience_A','Experience_B','Experience_C','Experience_D',
+           'Experience_E','Experience_F','Experience_G','Experience_H'],axis=1)
+y=df['Score']
+
+
+# Build random forest model
+rs=70
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+X, y, test_size=0.2, random_state=rs)
+# clf = RandomForestClassifier()
+# clf.fit(X, Y)
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+neighbors = []
+error = []
+for i in range(1,101):
+    neighbors.append(i)
+    knn_model = KNeighborsRegressor(n_neighbors=i,weights = 'distance')
+    knn_model.fit(X_train, y_train)
+    y_pred_knn = knn_model.predict(X_test)
+    error.append(mean_absolute_error(y_test,y_pred_knn))
+err = tuple(zip(neighbors,error))
+df_err = pd.DataFrame(err, columns = ['neighbors','error'])
+df_err[df_err.error == min(df_err.error)]
+for i in range(len(error)):
+    if error[i-1] == min(error):
+        t=i
+
+clf = KNeighborsRegressor(n_neighbors=26,weights = 'distance')
+clf.fit(X_train, y_train)
+
+
+def evaluatemodel(true,predicted,modelname):
+    MAE = round(mean_absolute_error(true,predicted),3)
+    print("\tMAE:", MAE)
+    
+y_pred_knn = knn_model.predict(X_test)
+evaluatemodel(y_test,y_pred_knn,'K-Nearest Neighbors')
+
+
+
+# Saving the model
+import pickle
+pickle.dump(clf, open('model.pkl', 'wb'))
+
+
+#******************************************************************************#
---
+++ Risk_Score_App.py
@@ -0,0 +1,150 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+import math
+from sklearn import neighbors
+from sklearn import linear_model
+from sklearn import svm
+import statsmodels.formula.api as smf
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+
+st.write("""
+# Risk Tolerance Score Prediction App
+This app predicts the **Risk Tolerance** score!
+Data obtained from UEL-ers in Python by ***Group 35***.
+""")
+
+st.sidebar.subheader('User Input Features')
+
+st.sidebar.markdown("""
+[Example CSV input file](https://docs.google.com/spreadsheets/d/192iJyTBJWstQ3ReTLvqMUXBSemrfJLuFHJSzwb_6rU0/edit?usp=sharing)
+""")
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        Age = st.sidebar.selectbox('Age',('A','B','C'))
+        Gender = st.sidebar.selectbox('Gender',('A','B'))
+        Marital = st.sidebar.selectbox('Marital',('A','B'))
+        Income = st.sidebar.selectbox('Income',('A','B','C'))
+        Ratio = st.sidebar.selectbox('Ratio',('A','B','C'))
+        Education = st.sidebar.selectbox('Education',('A','B','C'))
+        Profession = st.sidebar.selectbox('Profession',('A','B','C'))
+        Optimism = st.sidebar.selectbox('Optimism',('A','B'))
+        Objective = st.sidebar.selectbox('Source',('A','B','C'))
+        data = {'Age': Age,
+                'Gender': Gender,
+                'Marital': Marital,
+                'Income': Income,
+                'Ratio': Education,
+                'Profession': Profession,
+                'Optimism': Optimism,
+                'Objective': Objective}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+# Combines user input features with entire penguins dataset
+# This will be useful for the encoding phase
+penguins_raw = pd.read_csv('C:/Users/ACER/data_raw.csv')
+penguins = penguins_raw.drop(columns=['Score','Unnamed: 0'])
+df = pd.concat([input_df,penguins],axis=0)
+
+# Encoding of ordinal features
+encode = ['Age','Gender','Marital','Income','Ratio','Education','Profession','Optimism','Objective']
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+df = df[:1] # Selects only the first row (the user input data)
+
+# Displays the user input features
+st.header('Questions')
+st.subheader('Age: Bạn bao nhiêu tuổi rồi ?')
+st.markdown('  A. <18 tuổi')
+st.markdown('  B. 18-25 tuổi')
+st.markdown('  C. <18 tuổi')
+
+st.subheader('Gender: Giới tính của bạn ?')
+st.markdown('  A. Nam')
+st.markdown('  B. Nữ')
+
+st.subheader('Marital: Tình trạng hôn nhân của bạn ?')
+st.markdown('  A. Độc thân (chưa kết hôn hoặc đã ly hôn)')
+st.markdown('  B. Đã kết hôn')
+
+st.subheader('Income: Thu nhập cá nhân hàng tháng của bạn nằm trong khoảng nào ?')
+st.markdown('  A. <3tr')
+st.markdown('  B. 3tr-12tr')
+st.markdown('  C. >12tr')
+
+st.subheader('Ratio: Bạn dành bao nhiêu phần trăm trong thu nhập để phục vụ cho mục đích đầu tư ?')
+st.markdown('  A. <20%')
+st.markdown('  B. 20%-50%')
+st.markdown('  C. >50%')
+
+st.subheader('Education: Trình độ học vấn của bạn ?')
+st.markdown('  A. Dưới đại học.')
+st.markdown('  B. Đại học.')
+st.markdown(' C. Sau đại học (thạc sĩ, tiến sĩ hoặc cao hơn).')
+
+st.subheader('Profession: Bạn đánh giá về kiến thức trong đầu tư của bạn thế nào?')
+st.markdown('  A. Không có/chưa có bất kỳ kinh nghiệm trong lĩnh vực đầu tư.')
+st.markdown('  B. Có ít kiến thức trong đầu tư. Chủ yếu học hỏi qua sách báo, Internet.')
+st.markdown('  C. Có nhiều kiến thức đầu tư, đã/đang hoạt động trong lĩnh vực liên quan đến đầu tư')
+
+st.subheader('Optimism: Bạn mong muốn mình sẽ là trường hợp nào trong 2 trường hợp sau ?')
+st.markdown('  A. Lợi suất nhận được từ các khoản đầu tư cao hơn ngân hàng một chút, ổn định.')
+st.markdown('  B. Lợi suất nhận được từ các khoản đầu tư có thể lỗ nhiều hoặc lãi nhiều, không ổn định.')
+
+st.subheader('Source: Nguồn thu nhập chính của bạn là gì ?')
+st.markdown('  A. Lương công việc chính.')
+st.markdown('  B. Đầu tư.')
+st.markdown('  C. Khác.')
+
+
+
+st.title('User Input features')
+
+
+
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
+
+# Reads in saved classification model
+load_clf = pickle.load(open('model.pkl', 'rb'))
+
+# Apply model to make predictions
+prediction = load_clf.predict(df)
+
+
+st.header('Prediction of Risk Tolerance Score')
+st.write(prediction)
+st.write('---')
+
+def classify(prediction):
+    if load_clf.predict(df) <= 18:
+        st.write("Low tolerance for risk")
+    elif load_clf.predict(df) >= 19 and load_clf.predict(df)<=22:
+        st.write("Below-average tolerance for risk")
+    elif load_clf.predict(df) >= 23 and load_clf.predict(df)<=28:
+           st.write("Average/moderate tolerance for risk")
+    elif load_clf.predict(df) >= 29 and load_clf.predict(df)<=32:
+           st.write("Above-average tolerance for risk")         
+    else:
+        st.write("High tolerance for risk")
+        
+st.header('Level')
+classify(prediction)
+st.write('---')
