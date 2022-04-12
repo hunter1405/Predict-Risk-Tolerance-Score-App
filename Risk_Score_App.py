6e53c30cbdf3061cf49aac75373d4a40b6decaf7
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-12 21:16:36 +0700

    Update Risk_Score_App.py

--- Risk_Score_App.py
+++ Risk_Score_App.py
@@ -11,7 +11,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">UEL Fintech</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
