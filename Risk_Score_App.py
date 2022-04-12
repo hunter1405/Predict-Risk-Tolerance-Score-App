1c89260531955af674c89768ad30ad75e8a86ef0
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-12 07:36:17 +0700

    Update Risk_Score_App.py

--- Risk_Score_App.py
+++ Risk_Score_App.py
@@ -16,7 +16,7 @@ Data obtained from **UEL-ers** in Python by ***Group 35***.
 
 st.image(
     "https://www.plan-wisely.com/wp-content/uploads/sites/2/2020/07/Risk.png",
-    width=500,
+    width=700,
 )
 
 st.sidebar.subheader('User Input Features')
