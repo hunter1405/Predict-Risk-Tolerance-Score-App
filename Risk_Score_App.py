49b13760390f28068913f335c605d36cb5771bda
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-12 23:48:56 +0700

    Update Risk_Score_App.py

--- Risk_Score_App.py
+++ Risk_Score_App.py
@@ -32,7 +32,7 @@ div.fullScreenFrame > div {
 </style>""", unsafe_allow_html=True)
 
 #Title
-original_title = '<p style="font-family:sans-serif; color:#3498DB; text-shadow: 2px 2px 4px #000000; font-size: 50px;">Risk Tolerance Score Prediction App</p>'
+original_title = '<p style="text-align: center; color:#3498DB; text-shadow: 2px 2px 4px #000000; font-size: 50px;">Risk Tolerance Score Prediction App</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 # st.title("**Risk Tolerance Score Prediction App**")
 
