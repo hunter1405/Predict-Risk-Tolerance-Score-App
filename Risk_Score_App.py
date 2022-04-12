926fca0b56e29152f4a0a9feced72765e6d2aa82
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-12 18:53:57 +0700

    Update Risk_Score_App.py

--- Risk_Score_App.py
+++ Risk_Score_App.py
@@ -6,6 +6,8 @@ from PIL import Image
 
 st.set_page_config(layout="wide")
 
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
 h = st.markdown("""
 <style>
 div.fullScreenFrame > div {
