12fb8a47591466386aae0e5f7b1ab5aad3e73611
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-12 20:48:36 +0700

    Update Risk_Score_App.py

--- Risk_Score_App.py
+++ Risk_Score_App.py
@@ -32,8 +32,6 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
-lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
-img_contact_form = Image.open("images/yt_contact_form.png")
 img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
 
