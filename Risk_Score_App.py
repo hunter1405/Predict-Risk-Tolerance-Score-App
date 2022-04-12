94b77e66cddb40ae95a3c560af7720b3c5679563
hunter1405 <91263297+hunter1405@users.noreply.github.com>
2022-04-12 18:28:13 +0700

    Update Risk_Score_App.py

--- Risk_Score_App.py
+++ Risk_Score_App.py
@@ -128,12 +128,8 @@ load_clf = pickle.load(open('model.pkl', 'rb'))
 prediction = load_clf.predict(df)
 m = st.markdown("""
 <style>
-div.stButton > button:first-child { 
-color: #4F8BF9;
-border-radius: 20%;
-backgroud-color: #00ff00;
-height: 3em;
-width: 3em; 
+div.stButton > button:first-child {
+background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
 }
 </style>""", unsafe_allow_html=True)
 if st.button("Submit this form"):
